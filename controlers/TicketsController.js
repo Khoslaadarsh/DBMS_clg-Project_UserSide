e2b64e163d85ab2a2203ff3869d34b8eca48ea0b
Adarsh Kumar Khosla <khoslaadarsh@gmail.com>
2022-04-28 15:49:32 +0530

    booking and cancellation from both admin and user side

--- controlers/TicketsController.js
+++ controlers/TicketsController.js
@@ -10,17 +10,25 @@ function book(req, res) {
         DateOfBoarding: req.body.date,
         Email: req.body.email,
         numebr: req.body.number,
-        nameOfPassenger: req.body.fname
+        nameOfPassenger: req.body.fname,
+        Boardind_From: req.body.from,
+        Boardind_To: req.body.to
     };
     datainticket.UserId = JSON.stringify(datainticket.UserId);
     return new Promise((resolve, reject) => {
         connection.query(
             `
+            set autocommit = off;
             Start Transaction;
-            insert into Tickets set ?; 
+            set @from = (select Station_ID from Station where Name = '${datainticket.Boardind_From}');
+            set @to = (select Station_ID from Station where Name = '${datainticket.Boardind_To}');
+            insert into Tickets (Fare, TrainId, UserId, DateOfBoarding, Email, numebr, nameOfPassenger, Boardind_From, Boardind_To)
+            Values (${datainticket.Fare}, ${datainticket.TrainId}, ${datainticket.UserId}, "${datainticket.DateOfBoarding}", "${datainticket.Email}", ${datainticket.numebr}, "${datainticket.nameOfPassenger}", (select @from), (select @to))
+            ;
             UPDATE Train SET Seat_Available = Seat_Available-1 WHERE Train_No = ${req.body.tno};
             commit;
-            `, datainticket,
+            set autocommit = true;
+            `,
             (err, rows, cols) => {
                 if (err) {
                     reject(err);
--- controlers/deleteTicket.js
+++ controlers/deleteTicket.js
@@ -2,14 +2,21 @@ const connection = require("../database/connection");
 
 
 function deleteTicket(req) {
+
     return new Promise((resolve, reject) => {
         connection.query(
             `
+            set autocommit = false;
             start transaction;
+            insert into CanceledTicket (UserId, TicketId, Fare, DateOfBoarding, Email, numebr, nameOfPassenger , Boardind_From , Boardind_To , TrainId) select UserId , TicketId , Fare , DateOfBoarding, Email , numebr , nameOfPassenger , Boardind_From , Boardind_To , TrainId from Tickets 
+            where TicketId = ${req.params.id};
+
             delete from Tickets where TicketId = ${req.params.id};
             UPDATE Train SET Seat_Available = Seat_Available+1 WHERE Train_No = ${req.params.tno};
+            
             commit;
-            `,
+            set autocommit = true;
+            ` ,
             (err, rows, cols) => {
                 if (err) {
                     reject(err);
